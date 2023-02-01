fa58d5aae19313abf91438b546c57010533d40c1
mauroPh <mauro.philipe.santos@gmail.com>
2023-02-01 17:10:26 -0300

    project createdD

---
+++ src/components/OlaMundo.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1>Olá mundo!</h1>
+    <h2> Olá VUE !</h2>
+    <img src="@/assets/usar.png"/>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue';
+    
+    export default defineComponent({
+        name:"OlaMundo"
+    })
+</script>
+
+<style>
+   h1{
+    color: blue;
+   }
+</style>
+
+
